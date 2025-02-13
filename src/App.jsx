aac14caa831651e2abd8ee7444e8991c6aee8b0c
Nuel-Agyei <nuelagyei992@gmail.com>
2025-02-13 14:44:34 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Popup from './components/Popup'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Popup/>
+      </div>
+    </>
+)}
+
+export default App
